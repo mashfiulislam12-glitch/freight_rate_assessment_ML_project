d36cabc3a9c689993b4f6d14fe1adf6e09c23880
Mahi <mashfiulislam12@gmail.com>
2026-08-08 02:43:08 +0600

    logger and exeption added

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from datetime import datetime
+
+# Define log file name format with timestamp
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Define directory path for logs
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)
+
+# Full path to the log file
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Configure logging settings
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
